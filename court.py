5c896c4714afbf3d4b9274e6192ce3f63d8a3f69
blin4504 <bl4504@g.rit.edu>
2022-09-08 12:26:11 -0400

    added play, done, and info

--- court.py
+++ court.py
@@ -8,12 +8,12 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-court_1 = court_2 = court_3 = court_4 = []
+courts = [[], [], [], []]
 queue = []
 intents=discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 @bot.command()
 async def add(ctx, *, arg):
@@ -21,12 +21,36 @@ async def add(ctx, *, arg):
     queue.extend(parsed)
     people = ' '.join(queue)
     await ctx.send(f'added {people}')
-    
 
 @bot.command(name='queue')
 async def _queue(ctx):
-    peoples = '\n'.join(queue)
-    await ctx.send(peoples)
+    if (len(queue) > 0):
+        peoples = '\n'.join(queue)
+        await ctx.send(peoples)
+    else:
+        await ctx.send('No one is on queue')
+
+@bot.command()
+async def play(ctx):
+    if (len(queue) < 4):
+        await ctx.send('Not enough for a game')
+        return
+
+    for court in courts:
+        if (len(court) < 4):
+            for i in range(4):
+                court.append(queue[i])
+            del queue[:4]
+            return
+
+@bot.command()
+async def done(ctx, arg):
+    courts[int(arg)-1].clear()
+    await ctx.send(f'Court {arg} is open for another game')
+
+@bot.command()
+async def info(ctx):
+    for court in courts:
+        await ctx.send(court)
 
 bot.run(TOKEN)
-
